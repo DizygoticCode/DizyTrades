ecfc19bbbce4726d6e20a4382bda26bada25a027
DizygoticCode <rob.noyce@gmail.com>
2026-08-01 03:02:27 +0100

    Wire DizyBrain into terminal topbar

--- app/terminal/page.tsx
+++ app/terminal/page.tsx
@@ -1,5 +1,6 @@
 import { requireUser } from "../lib/auth";
 import { DizyBrainShell } from "../dizybrain-shell";
+import { DizyBrainTopbarLink } from "../dizybrain-topbar-link";
 import TradingTerminal from "../trading-terminal";
 
 export const dynamic = "force-dynamic";
@@ -8,6 +9,7 @@ export default async function TerminalPage() {
   const user = await requireUser();
   return (
     <DizyBrainShell>
+      <DizyBrainTopbarLink />
       <TradingTerminal user={user} />
     </DizyBrainShell>
   );
