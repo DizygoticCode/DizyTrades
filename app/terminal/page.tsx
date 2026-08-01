54aa874b70b31bf0c955e8f66016238f2515aed1
DizygoticCode <rob.noyce@gmail.com>
2026-08-01 02:45:42 +0100

    Wrap terminal with DizyBrain workspace

--- app/terminal/page.tsx
+++ app/terminal/page.tsx
@@ -1,9 +1,14 @@
 import { requireUser } from "../lib/auth";
+import { DizyBrainShell } from "../dizybrain-shell";
 import TradingTerminal from "../trading-terminal";
 
 export const dynamic = "force-dynamic";
 
 export default async function TerminalPage() {
   const user = await requireUser();
-  return <TradingTerminal user={user} />;
+  return (
+    <DizyBrainShell>
+      <TradingTerminal user={user} />
+    </DizyBrainShell>
+  );
 }
