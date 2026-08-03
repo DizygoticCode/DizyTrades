8d1d4da12630f34320a03748c1d3dc5aca377f41
DizygoticCode <rob.noyce@gmail.com>
2026-08-03 15:47:22 +0100

    Mount structure recovery guidance

--- app/structure/page.tsx
+++ app/structure/page.tsx
@@ -1,9 +1,15 @@
 import { requireUser } from "../lib/auth";
+import { WorkspaceStatePolish } from "../workspace-state-polish";
 import StructureClient from "./structure-client";
 
 export const dynamic = "force-dynamic";
 
 export default async function StructurePage() {
   const user = await requireUser();
-  return <StructureClient readOnly={user.role === "viewer"} userName={user.name}/>;
+  return (
+    <>
+      <WorkspaceStatePolish workspace="structure" />
+      <StructureClient readOnly={user.role === "viewer"} userName={user.name}/>
+    </>
+  );
 }
