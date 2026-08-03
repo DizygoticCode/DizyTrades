2ea6326845585caa91f8c8d0fe220c618a1c2d14
DizygoticCode <rob.noyce@gmail.com>
2026-08-03 15:47:00 +0100

    Mount terminal recovery guidance

--- app/terminal/page.tsx
+++ app/terminal/page.tsx
@@ -2,6 +2,7 @@ import { requireUser } from "../lib/auth";
 import { DizyBrainShell } from "../dizybrain-shell";
 import { DizyBrainTopbarLink } from "../dizybrain-topbar-link";
 import { FirstRunOnboarding } from "../first-run-onboarding";
+import { WorkspaceStatePolish } from "../workspace-state-polish";
 import TradingTerminal from "../trading-terminal";
 
 export const dynamic = "force-dynamic";
@@ -12,6 +13,7 @@ export default async function TerminalPage() {
     <DizyBrainShell>
       <DizyBrainTopbarLink />
       <FirstRunOnboarding userId={user.id} userName={user.name} />
+      <WorkspaceStatePolish workspace="terminal" />
       <TradingTerminal user={user} />
     </DizyBrainShell>
   );
