c0e5702855b6f9c221f3a24148efe0c3fa9f94b4
DizygoticCode <rob.noyce@gmail.com>
2026-08-03 15:47:13 +0100

    Mount scanner recovery guidance

--- app/scanner/page.tsx
+++ app/scanner/page.tsx
@@ -1,9 +1,15 @@
 import { requireUser } from "../lib/auth";
+import { WorkspaceStatePolish } from "../workspace-state-polish";
 import ScannerClient from "./scanner-client";
 
 export const dynamic = "force-dynamic";
 
 export default async function ScannerPage() {
   const user = await requireUser();
-  return <ScannerClient readOnly={user.role === "viewer"} userName={user.name}/>;
+  return (
+    <>
+      <WorkspaceStatePolish workspace="scanner" />
+      <ScannerClient readOnly={user.role === "viewer"} userName={user.name}/>
+    </>
+  );
 }
