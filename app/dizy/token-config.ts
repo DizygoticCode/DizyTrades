5f733afbdf7a8015441842a5636cfcf9d3b3def6
DizygoticCode <rob.noyce@gmail.com>
2026-08-11 14:46:38 +0100

    Add shared DIZY token configuration

---
+++ app/dizy/token-config.ts
@@ -0,0 +1,3 @@
+export const DIZY_MINT = "J9Bevbd4BS23cjoWbKazG1LGwRsAhr2iRQq6uo31BEaY";
+export const DIZY_POOL = "2mH8umwN2FfEx23bzTUuTXjQZ5G9rLNuJ2VWEkgynowA";
+export const DIZY_TOKEN_LOGO_URL = "https://gateway.irys.xyz/RAcoiCCewukn5Q9JMnHoMQK3nB8oFqrucA9GRTpNg16";
